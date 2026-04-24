7658b2298047634bda4468c8c2b34db7cf5e195f
DESKTOP-GEQQNQJ\serg2 <serg27makar@gmail.com>
2026-04-24 20:00:12 +0300

    Fixed router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import LoginView from '@/views/LoginView.vue'
 import DefaultLayout from '@/layouts/DefaultLayout.vue'
@@ -52,7 +52,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
