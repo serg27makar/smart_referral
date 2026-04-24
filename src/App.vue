1872ce60e8f85d3c8c9c930355e2dab0c12564af
serg27makar <46068111+serg27makar@users.noreply.github.com>
2026-04-24 09:03:21 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    Header
+  </header>
+
+  <main>
+    Boddy
+  </main>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
